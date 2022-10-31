3518bdd1b2b8097c0711b17627841b6b06470e6b
joyel <geolrco124@hotmail.com>
2022-10-31 12:56:24 -0500

    Project Created

---
+++ db.js
@@ -0,0 +1,5 @@
+const users=require("./data/users");
+
+module.exports={
+    users
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+server.db=router.db;
+
+server.use(auth);
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
